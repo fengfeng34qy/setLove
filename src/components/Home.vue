8a6e5996054755da3868c62a2ab211bc0f73ed0a
孙锋锋 <sunfengfeng@agree.com.cn>
2020-11-16 18:18:26 +0800

    初始化仓库

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,83 @@
 <template>
   <div class="Home">
+    <div class="title">设置</div>
     <div>
-      <div>
+      <div class="item-box">
         <el-input class="hersname" v-model="hersValue" size="small" placeholder="请输入内容">
           <template class="hersname-template" slot="prepend">需要表白的人</template>
         </el-input>
       </div>
-      <div>
+      <div class="item-box">
         <el-input class="yourname" v-model="yourValue" size="small" placeholder="请输入内容">
-          <template slot="prepend">你的姓名</template>
+          <template slot="prepend">你的名字</template>
         </el-input>
       </div>
-      <div>
-        <el-button type="primary" @click="send">确定</el-button>
+      <div class="item-box">
+        <el-input class="yourname" v-model="content" size="small" placeholder="正在开发中..." :disabled="true">
+          <template slot="prepend">表白内容</template>
+        </el-input>
       </div>
+      <div class="item-box">
+        <div>
+          <el-input class="yourname" v-model="content" size="small" :placeholder="bgName" :disabled="true">
+            <template slot="prepend">背景音乐</template>
+          </el-input>
+        </div>
+        <div style="margin-left:6px;display:none;">
+          <el-upload
+            class="upload-demo"
+            action="https://www.sunfengfeng.com/upload"
+            :http-request="getFileMsg"
+            :before-upload="onBeforeUpload"
+            :on-change="handleChange"
+            :on-success="onSuccess"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            :multiple="false"
+            :on-exceed="handleExceed"
+            :disabled="true"
+            :show-file-list="false">
+            <!-- <el-button size="small" type="success" :disabled="true" >选择音乐</el-button> -->
+          </el-upload>
+        </div>
+      </div>
+      <div class="item-box flex">
+        <div>
+          <el-input class="yourname" v-model="content" size="small" placeholder="暂不支持" :disabled="true">
+            <template slot="prepend">红包设置</template>
+          </el-input>
+        </div>
+        <div style="margin-left:6px;">
+          <el-upload
+            class="upload-demo"
+            action="https://www.sunfengfeng.com/upload"
+            :http-request="getFileMsg"
+            :before-upload="onBeforeUpload"
+            :on-change="handleChange"
+            :on-success="onSuccess"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            :multiple="false"
+            :on-exceed="handleExceed"
+            :disabled="true"
+            :show-file-list="false">
+            <el-button size="small" type="success" :disabled="true">支付红包</el-button>
+          </el-upload>
+        </div>
+      </div>
+      <div class="seq-box">
+        <div v-if="seqNo">
+          <div class="seq-tip">请牢记此序列号</div>
+          <div class="seq-no">{{seqNo}}</div>
+          <el-button v-clipboard:copy="seqNo" v-clipboard:success="copy">复制序列号</el-button>
+        </div>
+      </div>
+      <div class="">
+        <el-button type="primary" :disabled="isDisabled" size="medium" @click="send">确定</el-button>
+      </div>
+      <viewer class="zsm" :images="images">
+        <img v-for="src in images" :src="src" :key="src" style="width:100%;height:100%">
+      </viewer>
     </div>
   </div>
 </template>
@@ -25,17 +89,120 @@ export default {
   name: 'Home',
   data () {
     return {
+      images: [
+        '/static/images/zsm.jpg'
+      ],
+      seqNo: '',
+      bgName: '风雨无阻.mp3',
+      // fileList: [],
       hersValue: '',
-      yourValue: ''
+      yourValue: '',
+      content: ''
+    }
+  },
+  computed: {
+    isDisabled () {
+      let hersValue = this.hersValue.trim()
+      let yourValue = this.yourValue.trim()
+      return !(hersValue && yourValue)
     }
   },
   methods: {
+    copy () {
+      this.$message({
+        message: '成功复制到粘贴板！',
+        type: 'success'
+      })
+    },
     send () {
-      console.log('sds')
-      axios.post('http://www.sunfengfeng.com/createBiaobai', {})
+      const config = {
+        headers: {
+          // 'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      }
+      const loading = this.$loading({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(255, 255, 255, 0)'
+      })
+      axios.post('http://www.sunfengfeng.com/createBiaobai', {
+        biaobai: {
+          hersname: this.hersValue,
+          yourname: this.yourValue,
+          bgurl: 'http://www.sunfengfeng.com/mp3/fywz.mp3'
+        }
+      }, config)
         .then((res) => {
           console.log(res)
+          loading.close()
+          this.hersValue = ''
+          this.yourValue = ''
+          this.$message({
+            message: '已生成序列号',
+            type: 'success'
+          })
+          this.seqNo = res.data.seqNo
         })
+        .catch((error) => {
+          console.log(error)
+          loading.close()
+        })
+    },
+    getFileMsg (content) {
+      console.log('getFileMsg', content)
+      let reader = new FileReader()
+
+      reader.readAsDataURL(content.file)
+      reader.onload = () => {
+        var imgResult = reader.result
+        // console.log(imgResult)
+        var fd = new FormData()
+        // var base64data = reader.result
+        let base64data = imgResult.split(',')[1]
+        console.log(base64data)
+        let a = this.base64ToUint8Array(base64data)
+        console.log(a)
+        fd.append('rawaudio', a)
+        console.log(fd)
+        axios({
+          method: 'post',
+          url: 'http://www.sunfengfeng.com/upload',
+          data: {
+            name: content.file.name,
+            size: content.file.size,
+            type: content.file.type,
+            uid: content.file.uid,
+            lastModified: content.file.lastModified,
+            // file: a
+            data: base64data
+          },
+          timeout: 180000
+          // data: imgResult.replace('data:audio/mpeg;base64,', '')
+        }).then((res) => {
+          console.log(res)
+        })
+      }
+    },
+    handleRemove (file) {
+      console.log(file)
+    },
+    onBeforeUpload (file) {
+      console.log('上传之前', file)
+    },
+    handleChange (file, fileList) {
+      console.log(file, fileList)
+      this.bgName = file.name
+    },
+    onSuccess (file) {
+      console.log(file)
+    },
+    handleExceed (files, fileList) {
+      this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
+    },
+    beforeRemove (file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
     }
   }
 }
@@ -52,4 +219,31 @@ export default {
 }
 </style>
 <style scoped>
+.item-box {
+  margin: 30px 0;
+}
+.title {
+  font-size: 22px;
+  font-weight: 400px;
+}
+.seq-box {
+  height: 150px;
+}
+.seq-no {
+  font-size: 28px;
+  font-weight: 400;
+  margin: 10px;
+  color: #e0aa10;
+}
+.seq-tip {
+  font-size: 22px;
+  font-weight: 400;
+}
+.zsm {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
+import VueClipboard from 'vue-clipboard2'
+import Viewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Element)
+
+Vue.use(Viewer, {
+  defaultOptions: {zIndex: 9999, 'button': false, 'toolbar': false, 'navbar': false, title: false}
+})
+
+Vue.use(VueClipboard)
 
 /* eslint-disable no-new */
 new Vue({
